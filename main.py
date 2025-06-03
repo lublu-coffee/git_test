b0d009943222c341d01a532c19d468943f3c6831
Tamiron <tamipost@gmail.com>
2025-06-04 00:31:18 +0300

    Добавление методов для соединения залов с кинотеатрами: Cinema -> full_halls_lst Hall -> get_cinema

--- main.py
+++ main.py
@@ -3,18 +3,48 @@ from dataclasses import dataclass, field
 
 @dataclass
 class Cinema:
-    Id: int
+    count_id = 0
+    Id: int = field(init=False)
     Title: str
     Address: str
     CountHalls: int
-    Halls: field(default_factory=list)
+    Halls: list = field(default_factory=list)
+
+    def __post_init__(self):
+        Cinema.count_id += 1
+        self.Id = Cinema.count_id
+
+    def full_halls_lst(self, halls):
+        for h in halls:
+            self.Halls.append(h)
+            h.get_cinema(self)
 
 
 @dataclass
 class Hall:
-    Id: int
+    count_id = 0
+    Id: int = field(init=False)
     Title: str
-    Cinema: Cinema
-    Row: int
-    Seats: int
+    Row: int = 0
+    Seats: int = 0
+    Cinema: Cinema = None
 
+    def __post_init__(self):
+        Hall.count_id += 1
+        self.Id = Hall.count_id
+
+    def get_cinema(self, cinema):
+        self.Cinema = cinema
+
+
+h1 = Hall('A2', 20, 10)
+h2 = Hall('B4', 30, 15)
+print(h1.__dict__)
+print(h2.__dict__)
+
+c1 = Cinema('Кинотеатр 1', 'Баумана 10', 2)
+c1.full_halls_lst([h1, h2])
+print()
+print(h1.__dict__)
+print(h2.__dict__)
+print(c1.__dict__)
