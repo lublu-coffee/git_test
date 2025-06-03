ed31a102d516ab990449f609cfa5ee410b500c8f
Tamiron <tamipost@gmail.com>
2025-06-03 23:49:21 +0300

    функция get_int_range с продолжением ввода по 'y'

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-for x in range(10):
-    print(x)
+def get_int_range():
+    n = int(input('Введите для второго цикла: '))
+    for v2 in range(n):
+        print(v2)
+    if input('Хотите продолжить ') == 'y':
+        get_int_range()
 
-n = int(input('ВВедите для второго цикла'))
-for v2 in range(n):
-    print(v2)
+
+get_int_range()
