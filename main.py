7293f62a85b34d3a7d9d5a6238fcfe12af2f0666
Tamiron <tamipost@gmail.com>
2025-06-04 00:35:47 +0300

    Убрано свойство CountHalls, вместо него метод get_count_hall
    Убран метод get_cinema в Hall для избежания ссылок классов друг на друга. Теперь только кинотеатры (Cinema) хранят ссылки на залы (Hall)

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ class Cinema:
     Id: int = field(init=False)
     Title: str
     Address: str
-    CountHalls: int
     Halls: list = field(default_factory=list)
 
     def __post_init__(self):
         Cinema.count_id += 1
         self.Id = Cinema.count_id
 
+    def get_count_hall(self):
+        return len(self.Halls)
+
     def full_halls_lst(self, halls):
         for h in halls:
             self.Halls.append(h)
-            h.get_cinema(self)
 
 
 @dataclass
@@ -27,24 +28,16 @@ class Hall:
     Title: str
     Row: int = 0
     Seats: int = 0
-    Cinema: Cinema = None
 
     def __post_init__(self):
         Hall.count_id += 1
         self.Id = Hall.count_id
 
-    def get_cinema(self, cinema):
-        self.Cinema = cinema
-
 
 h1 = Hall('A2', 20, 10)
 h2 = Hall('B4', 30, 15)
-print(h1.__dict__)
-print(h2.__dict__)
-
-c1 = Cinema('Кинотеатр 1', 'Баумана 10', 2)
+c1 = Cinema('Кинотеатр 1', 'Баумана 10')
 c1.full_halls_lst([h1, h2])
-print()
 print(h1.__dict__)
 print(h2.__dict__)
 print(c1.__dict__)
