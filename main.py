22ea872869c3d71a656f10ec4eee18b7b08caa60
Tami <sonyadrozdikova@gmail.com>
2025-06-04 18:54:17 +0300

    были реализованны классы Session и Genre. а также заполнены примеры данных и их взаимодействия между собой

--- main.py
+++ main.py
@@ -33,6 +33,8 @@ class Hall:
     def __post_init__(self):
         self.Id = add_id(self.__class__.__name__)
 
+    def get_count_seats(self):
+        return self.Row * self.Seats
 
 @dataclass
 class Address:
@@ -41,6 +43,7 @@ class Address:
 
     def __post_init__(self):
         self.Id = add_id(self.__class__.__name__)
+
 
 @dataclass
 class Film:
@@ -53,14 +56,61 @@ class Film:
     def __post_init__(self):
         self.Id = add_id(self.__class__.__name__)
 
+    def get_genres(self, *args):
+        self.Genres = list(args)
 
 
-h1 = Hall('A2', 20, 10)
-h2 = Hall('B4', 30, 15)
-c1 = Cinema('Кинотеатр 1')
+@dataclass
+class Genre:
+    Id: int = field(init=False)
+    Title: str
+
+    def __post_init__(self):
+        self.Id = add_id(self.__class__.__name__)
+
+
+@dataclass
+class Session:
+    Id: int = field(init=False)
+    Data: str
+    Time: str
+    Film: Film
+
+    def __post_init__(self):
+        self.Id = add_id(self.__class__.__name__)
+
+
+# Адреса
 adr1 = Address('Баумана 21')
+adr2 = Address('Адоратского 10а')
+adr3 = Address('Сибгата Хакима 40')
+
+# Залы
+h1 = Hall('A2', 20, 10)
+h2 = Hall('B4', 20, 15)
+h11 = Hall('A2', 30, 10)
+h22 = Hall('B4', 20, 15)
+h3 = Hall('D2', 20, 10)
+h4 = Hall('Y4', 10, 15)
+
+# Кинотеатры
+c1 = Cinema('Кинотеатр 1')
+c2 = Cinema('Кинотеатр 2')
+c3 = Cinema('Кинотеатр 3')
 c1.full_halls_lst([h1, h2])
 c1.get_address(adr1)
-print(h1.__dict__)
-print(h2.__dict__)
+
+# Фильмы
+f1 = Film('Шрек', '', 120, [Genre('приключение')])
+f2 = Film('Моана', '', 90, [Genre('приключение'), Genre('романтика')])
+f3 = Film('Миньоны 3', '', 59, [Genre('приключение'), Genre('боевик')])
+
+# Сеансы
+s1 = Session('2025-06-06', '12:00', f1)
+s2 = Session('2025-06-06', '13:30', f1)
+s3 = Session('2025-07-08', '11:10', f3)
+
+
 print(c1.__dict__)
+print(s1.__dict__)
+print(s2.__dict__)
